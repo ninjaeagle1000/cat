08d4426a2ff25729b3b23a4072bce82478faa6ed
ninjaeagle1000 <ninjaeagle1000@hotmail.com>
2026-09-09 10:41:59 -0400

    Commit completed .c file

---
+++ cat2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void open_and_print(int index, char **file_name);
+
+int main(int argc, char **argv) {
+	if (argc == 1) {
+		printf("Usage: cat2 <file1> (<other files>)\n");
+		return 1;
+	}
+
+	for (int i = 1; i < argc; i++) {
+		open_and_print(i, argv);
+	}
+
+	return 0;
+}
+
+void open_and_print(int index, char **file_name) {
+	FILE *fptr;
+
+	fptr = fopen(file_name[index], "r");
+	if (fptr == NULL) {
+		printf("ERROR: file not opened");
+		return;
+	}
+
+	while (!feof(fptr)) {
+		char letter = fgetc(fptr);
+		if (feof(fptr)) break;
+		putchar(letter);
+	}
+
+	fclose(fptr);
+
+	return;
+}
